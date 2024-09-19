9503f89887e7bd32fd1996b8d4234cd50512df6f
Nahidul007 <nahidulalam100@gmail.com>
2024-09-19 01:19:19 -0700

    this is my home functio layout

--- js/loging.js
+++ js/loging.js
@@ -2,7 +2,18 @@ document.getElementById('button-loging').addEventListener('click', function (eve
     event.preventDefault();
     console.log('loging button click');
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+    // console.log(phoneNumber, pinNumber);
+
+    // bad way to validate 
+    if (phoneNumber === '7' && pinNumber === '1234') {
+        console.log('You are loging');
+        window.location.href='/home.html';
+        
+    }
+    else{
+        alert('wrong phone number or pin')
+    }
     
     
 })
