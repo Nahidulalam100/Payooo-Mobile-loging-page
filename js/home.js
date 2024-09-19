88e7153f76c18dae9c64631442be795ccc4893ac
Nahidul007 <nahidulalam100@gmail.com>
2024-09-19 04:52:02 -0700

    Home section inpomplete

--- js/home.js
+++ js/home.js
@@ -1,1 +1,11 @@
+document.getElementById('btn-add-money').addEventListener('click', function (event) {
+    event.preventDefault();
 
+    const amountAdded = document.getElementById('amount-added').value ;
+    // console.log(amountAdded);
+    const pinNumberInpt = document.getElementById('input-pin-number').value;
+    // console.log(pinNumberInpt);
+    
+    
+    
+})
