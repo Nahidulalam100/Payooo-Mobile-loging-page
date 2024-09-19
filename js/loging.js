588a80b9f47410f259b8e4353f9c405b47301e71
Nahidul007 <nahidulalam100@gmail.com>
2024-09-18 23:57:43 -0700

    this is prevent reloading and preventdefault

---
+++ js/loging.js
@@ -0,0 +1,8 @@
+document.getElementById('button-loging').addEventListener('click', function (event) {
+    event.preventDefault();
+    console.log('loging button click');
+    const phoneNumber = document.getElementById('phone-number').value;
+    console.log(phoneNumber);
+    
+    
+})
