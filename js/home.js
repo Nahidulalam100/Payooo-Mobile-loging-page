9de0d69b1f8fd4160031daea8aff8162e0272de7
Nahidul007 <nahidulalam100@gmail.com>
2024-09-19 05:30:01 -0700

    Add money to the balance after pin verification

--- js/home.js
+++ js/home.js
@@ -1,10 +1,34 @@
 document.getElementById('btn-add-money').addEventListener('click', function (event) {
     event.preventDefault();
-
     const amountAdded = document.getElementById('amount-added').value ;
-    // console.log(amountAdded);
     const pinNumberInpt = document.getElementById('input-pin-number').value;
-    // console.log(pinNumberInpt);
+
+
+    // wrong way to validate pin  Number
+    if (pinNumberInpt === '1234') {
+        console.log('added money to account');
+        const blance = document.getElementById('acount-blance').innerText;
+        // console.log(blance);
+        const addMoneyNumber = parseFloat(amountAdded);
+        const addBlanceNumver = parseFloat(blance)
+        const newBlance = addMoneyNumber + addBlanceNumver ;
+
+
+        // Update the blance in the  ui/Dom
+        document.getElementById('acount-blance').innerText = newBlance;
+
+
+
+        
+        
+        
+        
+        
+        
+    }
+    else{
+        alert('Fail to added money try again')
+    }
     
     
     
